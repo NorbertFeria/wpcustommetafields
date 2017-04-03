120f8152926d45bce6753cf138ed87c7a531d5e1
norbert-feria-53 <norbert@loveclients.com>
2017-04-04 02:28:30 +0800

    install verified

--- uninstall.php
+++ uninstall.php
@@ -2,10 +2,10 @@
 
 if(defined('WP_UNINSTALL_PLUGIN') ){
 	
-	//global $wpdb;
-	//$wpdb->query( "DROP TABLE IF EXISTS {$wpdb->prefix}custom_metabox" );
+	global $wpdb;
+	$wpdb->query( "DROP TABLE IF EXISTS {$wpdb->prefix}custom_metabox" );
 
-	//$wpdb->query( "DROP TABLE IF EXISTS {$wpdb->prefix}custom_metabox_fields" );
+	$wpdb->query( "DROP TABLE IF EXISTS {$wpdb->prefix}custom_metabox_fields" );
 }
 
 ?>
